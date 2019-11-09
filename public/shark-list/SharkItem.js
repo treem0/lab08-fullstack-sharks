856b910c27c6df455e3e911c07992d194a46c6af
Travis Molter <travismolter@gmail.com>
2019-11-08 16:27:53 -0800

    trying to get dangerlevel to show

--- public/shark-list/SharkItem.js
+++ public/shark-list/SharkItem.js
@@ -13,7 +13,7 @@ class SharkItem extends Component {
             </div>
             <div class="info-container">
             <p class="fatality">Is the Shark a Killer?${shark.killer}</p>
-            <p class="dangerous">${shark.dangerLevel.dangerous}</p>
+            <p class="dangerous">${shark.dangerLevel}</p>
             </div>
         </li>
     `;
